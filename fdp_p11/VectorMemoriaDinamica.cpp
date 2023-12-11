002fe2b883ed340928d782ef61bc1f6410c4e033
jcv00036 <152983584+jcv00036@users.noreply.github.com>
2023-12-11 12:54:37 +0100

    Add files via upload

---
+++ fdp_p11/VectorMemoriaDinamica.cpp
@@ -0,0 +1,44 @@
+//@Title: VectorMemoriaDinamica
+//@Author: Juan Carrasco Vico
+//@Desc:  Realiza un programa que solicite al usuario un tamaño de vector y reserve memoria
+//dinámica para un vector de enteros con el tamaño indicado. A continuación rellene el
+//vector con valores solicitados al usuario. Realiza también una función que muestre el
+//contenido de un vector y utilízala para comprobar que los valores solicitados al usuario
+//se han leído correctamente.
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+string imprimirVector(int vector[],int tam){
+    stringstream sso;
+
+    sso << "[";
+    for (int i = 0; i < tam; ++i) {
+        sso << vector[i];
+        if (i<tam-1) sso << " , ";
+    }
+    sso << "]";
+
+    return sso.str();
+}
+
+int main(){
+
+    int tamIndicado;
+    cout << "De que tam quieres el vector?" << endl;
+    cin >> tamIndicado;
+
+    int *vector = new int[tamIndicado];
+
+    for (int i = 0; i < tamIndicado; ++i) {
+        cout << "Indique el valor de la posicion " << i <<":"<< endl;
+        cin >> vector[i];
+    }
+
+    cout << imprimirVector(vector,tamIndicado);
+
+    delete[] vector;
+    return 0;
+}
